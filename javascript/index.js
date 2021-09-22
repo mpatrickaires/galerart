3b1f96c651876b9726495e0345304488ab785416
Matheus Patrick Aires Lopes <mpatrickaires@gmail.com>
2021-09-22 18:11:33 -0300

    Added animation to modal show and an event to close modal when the ESC key is pressed

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,14 @@
 let artistElements = document.getElementsByClassName('artist');
+const modal        = document.getElementById('modal-art');
+const art          = document.getElementById('art');
 
 for (let i = 0; i < artistElements.length; i++) {
     artistElements[i].addEventListener('click', (event) => {
-        showModal(event.currentTarget.id, 'modal-art', 'art');
+        showModal(event.currentTarget.id);
     })
 }
 
-function showModal(artist, modalContainerID, modalArtID) {
-    const modal = document.getElementById(modalContainerID);
-    const art   = document.getElementById(modalArtID);
-
+function showModal(artist) {
     modal.classList.add('modal-show');
     modal.addEventListener('click', (event) => {
         if (event.target.id == modal.id || event.target.id == 'modal-close') {
@@ -17,8 +16,14 @@ function showModal(artist, modalContainerID, modalArtID) {
         }
     })
 
-    art.style.backgroundImage    = `url(../img/art-${artist}.jpg)`;
+    art.style.backgroundImage    = `url(img/art-${artist}.jpg)`;
     art.style.backgroundRepeat   = 'no-repeat';
     art.style.backgroundSize     = 'contain';
     art.style.backgroundPosition = 'center center';
+}
+
+document.onkeydown = function(evt) {
+    if (evt.key ==  'Escape') {
+        modal.classList.remove('modal-show');
+    }
 }
